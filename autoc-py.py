2b0d7d119d82fe41a81170d80381a6cfc87cefe4
parsa <psrzp1386@gmail.com>
2025-01-12 17:54:37 +0330

    hopefully working code

--- autoc-py.py
+++ autoc-py.py
@@ -1,9 +1,6 @@
 import json
-import threading
 import time
-from pynput import mouse, keyboard
-from pynput.mouse import Button, Controller
-from pynput.keyboard import Key, Listener
+import device_mgr
 
 # Load configuration from JSON file
 def load_config():
@@ -32,29 +29,27 @@ class AutoClicker:
         self.frequency = frequency
         self.randomize = randomize
         self.running = False
-        self.thread = None
         self.mouse = mouse.Controller()
 
     def start(self):
         self.running = True
-        self.thread = threading.Thread(target=self.click_loop)
-        self.thread.start()
+        print(f"AutoClicker started with trigger key '{self.trigger_key}'")
 
     def stop(self):
         self.running = False
-        if self.thread:
-            self.thread.join()
+        print(f"AutoClicker stopped for key '{self.trigger_key}'")
 
     def click_loop(self):
         delay = 1 / self.frequency
-        while self.running:
-            if self.randomize:
-                delay = 1 / self.frequency + (time.time() % 0.01)
-            if self.click_type in mouse_buttons:
-                self.mouse.click(mouse_buttons[self.click_type])
-            else:
-                self.press_key(self.click_type)
-            time.sleep(delay)
+        if self.randomize:
+            delay += (time.time() % 0.01)
+
+        if self.click_type in mouse_buttons:
+            self.mouse.click(mouse_buttons[self.click_type])
+        else:
+            self.press_key(self.click_type)
+
+        time.sleep(delay)
 
     def press_key(self, key):
         with keyboard.Controller() as kb:
@@ -106,7 +101,14 @@ def test_mouse_clicks():
     except Exception as e:
         print(f"An error occurred during the test: {e}")
 
+def run_auto_clicker():
+    while True:
+        for instance in instances.values():
+            if instance.running:
+                instance.click_loop()
+
 with Listener(on_press=on_press) as listener:
     test_mouse_clicks()
     print("AutoClicker is running. Press your configured keys to toggle.")
+    run_auto_clicker()
     listener.join()
---
+++ device_mgr.py
@@ -0,0 +1,49 @@
+import sys
+import platform
+
+if platform.system() == "Linux":
+    # Linux Wayland-compatible
+    from pynput.mouse import Button, Controller
+    from pynput.keyboard import Controller as KeyboardController
+    mouse = Controller()
+    keyboard = KeyboardController()
+else:
+    # Windows compatible
+    import pyautogui
+
+
+def right():
+    # Right-click (RMB)
+    if platform.system() == "Linux":
+        mouse.click(Button.right)
+    elif platform.system() == "Windows":
+        pyautogui.click(button="right")
+
+
+def left():
+    # Left-click (LMB)
+    if platform.system() == "Linux":
+        mouse.click(Button.left)
+    elif platform.system() == "Windows":
+        pyautogui.click(button="left")
+
+
+def middle():
+    # Middle-click (MMB)
+    if platform.system() == "Linux":
+        mouse.click(Button.middle)
+    elif platform.system() == "Windows":
+        pyautogui.click(button="middle")
+
+
+def type(string):
+    # Type something
+    if platform.system() == "Linux":
+        keyboard.type(string)
+    elif platform.system() == "Windows":
+        pyautogui.typewrite(string)
+
+
+if __name__ == "__main__":
+    type("fuck you")
+
